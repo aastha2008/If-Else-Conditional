ce9e1ef29e3a2be8a4803392b8fe562035fbd839
Bhagat Aastha <109658@eesd.org>
2021-01-25 23:05:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+if/else Conditional Statement
+01/21/2021
+
+-Used for decision-making operations with conditions.
+Conditional statement always starts with a if header.
+else header/clause is optional
+
+Condition:
+-An expression that evaluates to porduce a result which is a boolean vaule (Booleam expression).
+
+Indentation
+-Python telies on indentation to define scope in the code
+
+Formula:
+
+if(CONDITION):
+  BODY STATEMENT
+else:
+  BODY STATEMENT
+Ex)
+
+
+'''
+x=5
+if(x>3):
+  print("hello")
+else:
+  print("World")
